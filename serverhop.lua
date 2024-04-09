23cb28874b93a22560dffcc8daff1c6cffe6cc36
Mustwey <135557972+Mustwey@users.noreply.github.com>
2024-04-09 14:25:33 -0400

    Update serverhop.lua

--- serverhop.lua
+++ serverhop.lua
@@ -1,9 +1,5 @@
 local function alternateServersRequest()
-    local response = request({
-        Url = 'https://games.roblox.com/v1/games/' .. tostring(game.PlaceId) .. '/servers/Public?sortOrder=Desc&limit=100',
-        Method = "GET",
-        Headers = { ["Content-Type"] = "application/json" },
-    })
+    local response = request({Url = 'https://games.roblox.com/v1/games/' .. tostring(game.PlaceId) .. '/servers/Public?sortOrder=Desc&limit=100', Method = "GET", Headers = { ["Content-Type"] = "application/json" },})
 
     if response.Success then
         return response.Body
@@ -24,32 +20,20 @@ local function getServer()
         servers = game.HttpService:JSONDecode(alternateServersRequest()).data
     end
 
-    if servers and #servers > 0 then
-        local server = servers[Random.new():NextInteger(80, #servers)]
+    local server = servers[Random.new():NextInteger(80, 100)]
+    if server then
         return server
     else
-        warn("Failed to retrieve servers or no servers available.")
-        return nil
+        return getServer()
     end
 end
 
-local function tryTeleport()
-    local server = getServer()
-    if server then
-        game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, server.id, game.Players.LocalPlayer)
-    else
-        print("Retrying to get a server.")
-        tryTeleport() -- Recursive call to retry getting a server and teleporting
-    end
+pcall(function()
+    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, getServer().id, game.Players.LocalPlayer)
+end)
+
+task.wait(5)
+while true do
+    game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+    task.wait()
 end
-
-local function onTeleportInitFailed(player, teleportResult, errorMessage)
-    print("Teleport failed: ", errorMessage)
-    tryTeleport() -- Attempt to teleport again using a different server
-end
-
--- Connecting the TeleportInitFailed event
-game:GetService("TeleportService").TeleportInitFailed:Connect(onTeleportInitFailed)
-
--- Initial teleport attempt
-tryTeleport()
