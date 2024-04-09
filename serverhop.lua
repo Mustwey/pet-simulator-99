c08e75bb82eda2d47100327090b465d6e4510cf7
Mustwey <135557972+Mustwey@users.noreply.github.com>
2024-04-09 09:48:48 -0400

    Update serverhop.lua

--- serverhop.lua
+++ serverhop.lua
@@ -20,7 +20,7 @@ local function getServer()
         servers = game.HttpService:JSONDecode(alternateServersRequest()).data
     end
 
-    local server = servers[Random.new():NextInteger(100, 150)]
+    local server = servers[Random.new():NextInteger(5, 100)]
     if server then
         return server
     else
