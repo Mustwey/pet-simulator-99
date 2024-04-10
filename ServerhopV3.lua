1677c4fcf5f8168e94168566a6411dc96d0050bd
Mustwey <135557972+Mustwey@users.noreply.github.com>
2024-04-10 08:12:49 -0400

    Update ServerhopV3.lua

--- ServerhopV3.lua
+++ ServerhopV3.lua
@@ -1,56 +1,35 @@
-local a = 1
 local function alternateServersRequest()
     local response = request({Url = 'https://games.roblox.com/v1/games/' .. tostring(game.PlaceId) .. '/servers/Public?sortOrder=Desc&limit=100', Method = "GET", Headers = { ["Content-Type"] = "application/json" },})
 
     if response.Success then
         return response.Body
     else
-        warn("Failed to fetch servers: " .. response.StatusCode .. " " .. response.StatusMessage)
         return nil
     end
 end
 
-local function getServer(retryLimit)
+local function getServer()
     local servers
-    local retryCount = 0
-    local success = pcall(function()
+
+    local success, _ = pcall(function()
         servers = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. tostring(game.PlaceId) .. '/servers/Public?sortOrder=Desc&limit=100')).data
     end)
 
     if not success then
         print("Error getting servers, using backup method")
-        servers = game.HttpService:JSONDecode(alternateServersRequest()).data or nil
+        servers = game.HttpService:JSONDecode(alternateServersRequest()).data
     end
 
-    -- Ensure servers is not nil and has items
-    if servers and #servers > 0 then
-        while retryCount < retryLimit do
-            local randomIndex = math.random(50, #servers)
-            local server = servers[randomIndex]
-            if server and server.playing < server.maxPlayers then
-                return server
-            else
-                retryCount = retryCount + 1
-            end
-        end
-    end
-
-    if retryCount >= retryLimit then
-        warn("Retry limit reached, no suitable server found.")
-        return nil
-    end
+    local server = servers[Random.new():NextInteger(1, #servers)]
+    return server
 end
 
-while (a>6) do
-    local server = getServer(5) -- Attempt to get a suitable server 10 times
-    if server and server.id then
-        pcall(function()
-            game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, server.id, Players.LocalPlayer)
-        end)
-    else
-        warn("Unable to find a suitable server. Waiting before retrying...")
-    end
-    a += 1
-    task.wait(4) -- Use task.wait() if available, for better performance and reliability
-end
+pcall(function()
+    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, getServer().id, game.Players.LocalPlayer)
+end)
 
+task.wait(5)
+while true do
+    game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+    task.wait()
+end
