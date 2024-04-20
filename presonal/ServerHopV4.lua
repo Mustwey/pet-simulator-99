80681ef5f3b68a4e904e2422405b6c61055120da
Mustwey <135557972+Mustwey@users.noreply.github.com>
2024-04-20 17:22:56 -0400

    Update ServerHopV4.lua

--- presonal/ServerHopV4.lua
+++ presonal/ServerHopV4.lua
@@ -1,94 +1,38 @@
--- Importing Services and Initializing Variables
-local Players = game:GetService('Players')
-local LocalPlayer = Players.LocalPlayer
-local TeleportService = game:GetService("TeleportService")
-local GuiService = game:GetService("GuiService")
-local HttpService = game:GetService("HttpService")
-local StarterGui = game:GetService("StarterGui")
-local VirtualInputManager = game:GetService("VirtualInputManager")
-local CoreGui = game:GetService("CoreGui")
+queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/Mustwey/pet-simulator-99/main/autoBalloon.lua"))
+task.wait(1)
+local function alternateServersRequest()
+    local response = request({Url = 'https://games.roblox.com/v1/games/' .. tostring(game.PlaceId) .. '/servers/Public?sortOrder=Desc&limit=100', Method = "GET", Headers = { ["Content-Type"] = "application/json" },})
 
-local BlockUtils = {}
-local IsFriendWith = LocalPlayer.IsFriendsWith
-
--- Custom API for account management
-local Account = {}
-local WebserverSettings = {
-    Port = '7963',
-    Password = ''
-}
-
-function WebserverSettings:SetPort(Port) self.Port = Port end
-function WebserverSettings:SetPassword(Password) self.Password = Password end
-
-local function HTTPRequest(Method, URL)
-    local Response = syn.request({
-        Method = Method,
-        Url = URL
-    })
-    return Response.StatusCode == 200 and Response.Body or false
-end
-
-function Account.new(Username)
-    local self = setmetatable({}, Account)
-    self.Username = Username
-    return self
-end
-
-function Account:BlockUser(UserId)
-    local URL = 'http://localhost:' .. WebserverSettings.Port .. '/BlockUser?Account=' .. self.Username .. '&UserId=' .. UserId .. (WebserverSettings.Password and '&Password=' .. WebserverSettings.Password or '')
-    return HTTPRequest('GET', URL)
-end
-
-function Account:UnblockEveryone()
-    local URL = 'http://localhost:' .. WebserverSettings.Port .. '/UnblockEveryone?Account=' .. self.Username .. (WebserverSettings.Password and '&Password=' .. WebserverSettings.Password or '')
-    return HTTPRequest('GET', URL)
-end
-
-local apiAccount = Account.new(LocalPlayer.Name)
-
--- Utility Functions
-local function isFriendWith(userId)
-    local success, data = pcall(IsFriendWith, LocalPlayer, userId)
-    return success and data or true
-end
-
-function BlockUtils:BlockUser(userId)
-    return apiAccount:BlockUser(userId)
-end
-
-function BlockUtils:UnblockUser()
-    return apiAccount:UnblockEveryone()
-end
-
-function BlockUtils:BlockRandomUser()
-    for _, player in ipairs(Players:GetPlayers()) do
-        if player ~= LocalPlayer and not isFriendWith(player.UserId) then
-            self:BlockUser(player.UserId)
-            break
-        end
+    if response.Success then
+        return response.Body
+    else
+        return nil
     end
 end
 
--- Queue a script to run upon teleport
-queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/Mustwey/pet-simulator-99/main/autoBalloon.lua"))
-task.wait(1)
+local function getServer()
+    local servers
 
--- Check if the player arrived from a teleport
-local hasArrivedFromTeleport = TeleportService:GetLocalPlayerTeleportData() ~= nil
-print("Arrived from teleport:", hasArrivedFromTeleport)
+    local success, _ = pcall(function()
+        servers = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. tostring(game.PlaceId) .. '/servers/Public?sortOrder=Desc&limit=100')).data
+    end)
 
--- Decision based on teleport arrival
-if hasArrivedFromTeleport then
-    BlockUtils:UnblockUser()
-    wait(1)
-else
-    BlockUtils:BlockRandomUser()
-    wait(1)
+    if not success then
+        print("Error getting servers, using backup method")
+        servers = game.HttpService:JSONDecode(alternateServersRequest()).data
+    end
+
+    local server = servers[Random.new():NextInteger(1, #servers)]
+    return server
 end
 
--- Constantly attempt to teleport to a random place
+pcall(function()
+    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, getServer().id, game.Players.LocalPlayer)
+end)
+
+task.wait(1)
 while true do
-    TeleportService:Teleport(game.PlaceId, LocalPlayer)
+    game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
     task.wait()
 end
+
