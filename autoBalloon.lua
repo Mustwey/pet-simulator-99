b86e1e03be11f2eb9dcc0db34614e8a3e9bf37f2
Mustwey <135557972+Mustwey@users.noreply.github.com>
2024-04-20 17:20:47 -0400

    Update autoBalloon.lua

--- autoBalloon.lua
+++ autoBalloon.lua
@@ -1,11 +1,11 @@
 getgenv().autoBalloon = true
 
 getgenv().autoBalloonConfig = {
-    SERVER_MINIMUM_TIME = 45, -- minimum time to wait before server hopping
+    SERVER_MINIMUM_TIME = 35, -- minimum time to wait before server hopping
     START_DELAY = 2.5, -- delay before starting
     SERVER_HOP_AFTER_NOT_FIND = false, -- if the balloon isn't found, instead of checking through the rest of the balloons, it will just server hop
     SERVER_HOP_DELAY = 0, -- delay before server hopping
-    BALLOON_DELAY = 1, -- delay before popping next balloon (if there are multiple balloons in the server)
+    BALLOON_DELAY = 0.5, -- delay before popping next balloon (if there are multiple balloons in the server)
     GET_BALLOON_DELAY = 0, -- delay before getting balloons again if none are detected
     GIFT_BOX_BREAK_FAILSAFE = 1, -- seconds to wait before skipping gift boxes if they don't function properly
 }
